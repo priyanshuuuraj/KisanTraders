5e5c143ac2d9de65374021c77129d4d3e0bf31bb
Priyanshu Raj <rajpriyanhuuu@gmail.com>
2026-03-09 18:06:31 +0530

    fix db connection for serverless

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(`${process.env.MONGO_URI}/KisanTraders`)
-        console.log("MongoDB connected succesfully");
-            
-    }catch (error){
-        console.log("MongoDB connection failed:", error);
-    }
+let isConnected = false;
 
-            
-}
+const connectDB = async () => {
+  if (isConnected) return;
+  
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/KisanTraders`);
+    isConnected = true;
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.log("MongoDB connection failed:", error);
+  }
+};
 
-export default connectDB
+export default connectDB;
